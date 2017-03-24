99b17500d25a023ce55025f724987abf0f9cfe66
Alain Armand <aarmand.inbox@gmail.com>
2017-03-24 15:14:37 +0100

    configured MONGO_URL for azure

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
 export const expressPort = process.env.PORT || 3060
 
-export const MONGO_URL = "mongodb://localhost:27017/graphqldb"
+export const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/graphqldb"
