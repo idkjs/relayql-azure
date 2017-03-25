0546225842e38ec19e2c5a452346c23090a17d5c
Alain Armand <aarmand.inbox@gmail.com>
2017-03-25 12:07:11 +0100

    changed config port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-export const expressPort = process.env.PORT || 3060
+export const expressPort = process.env.PORT || 3000
 
 export const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/graphqldb"
